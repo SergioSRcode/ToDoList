b8e449b85cc44041cddb31dd9dcc750bc08cb0bd
Sergio Ravera <sergioravera@hotmail.de>
2024-10-29 16:25:34 +0100

    moved private method to the bottom and added some comments to the code

--- todolist.js
+++ todolist.js
@@ -114,12 +114,6 @@ class TodoList {
     return filteredList;
   }
 
-  _validateIndex(idx) {
-    if (!(idx in this.todos)) {
-      throw ReferenceError(`invalid index: ${idx}`);
-    }
-  }
-
   findByTitle(title) {
     return this.filter(todo => todo.title === title).first();
   }
@@ -148,39 +142,10 @@ class TodoList {
   toArray() {
     return this.todos.slice();
   }
+
+  _validateIndex(idx) {  // _ in name indicates "private" method
+    if (!(idx in this.todos)) {
+      throw ReferenceError(`invalid index: ${idx}`);
+    }
+  }
 }
-
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
-let list = new TodoList("Today's Todos");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-todo1.markDone();
-todo5.markDone();
-
-// let doneTodos = list.filter(todo => todo.isDone());
-// console.log(doneTodos);
-
-// console.log(list.filter(todo => todo.isDone()).first());
-// console.log(list.findByTitle("Clean room"));
-// console.log(list.findByTitle("Clean Room"));
-// console.log(list.allDone());
-// console.log(list.allNotDone());
-// console.log(list.markDone("Clean room"));
-// console.log(list.markAllDone());
-// console.log(list.allDone());
-// console.log(list.markAllUndone());
-// console.log(list.allDone());
-// console.log(list.allNotDone());
-console.log(list.toArray());
-console.log(list.toArray() === list.todos);  // false
